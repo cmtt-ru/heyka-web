f18793134b6feb1f4ad3c8a1133058d851581628
Michael Nalbandyan <xinger@ya.ru>
2020-12-17 15:46:32 +0300

    Update merge-in-dev.sh

--- bin/merge-in-dev.sh
+++ bin/merge-in-dev.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-printf "Merging \"$current_branch\" into \"develop\"..."
 # Get current branch name
 current_branch=$(git symbolic-ref -q HEAD)
 current_branch=${current_branch##refs/heads/}
@@ -12,6 +11,7 @@ git pull origin develop
 
 # Merge current branch to develop
 git merge $current_branch
+git push origin develop
 
 # Checkout current branch back
 git checkout $current_branch
