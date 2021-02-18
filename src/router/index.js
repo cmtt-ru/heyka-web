6ded5f8e292fc64886fbb60d3e29cbeda28440d4
Michael Nalbandyan <xinger@ya.ru>
2021-02-18 19:27:21 +0300

    remove old slack invite

--- src/router/index.js
+++ src/router/index.js
@@ -17,8 +17,6 @@ const AuthSocialLogin = () => import(/* webpackChunkName: "main" */ '../views/Au
 const AuthSocialCallback = () => import(/* webpackChunkName: "main" */ '../views/Auth/SocialCallback');
 const AuthSlackCallback = () => import(/* webpackChunkName: "main" */ '../views/Auth/SlackCallback');
 
-const SlackInvite = () => import(/* webpackChunkName: "main" */ '../views/Auth/SlackInvite.vue');
-
 const Guest = () => import(/* webpackChunkName: "main" */ '../views/Guest');
 const GuestStart = () => import(/* webpackChunkName: "main" */ '../views/Guest/Start');
 const GuestGrid = () => import(/* webpackChunkName: "main" */ '@sdk/views/Call/Grid');
