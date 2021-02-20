3a11197d547f602b1337a709061d6cbb634dfb37
Иван <bushmin@list.ru>
2021-02-20 16:56:58 +0300

    Update getters.js

--- src/store/getters.js
+++ src/store/getters.js
@@ -88,6 +88,30 @@ export default {
   },
 
   /**
+   * Get last user who shares screen
+   *
+   * @param {object} state – global state
+   * @param {object} getters – global getters
+   * @returns {null|string}
+   */
+  getUserWhoSharesScreen: (state, getters) => {
+    const selectedChannelId = getters['me/getSelectedChannelId'];
+    const selectedChannel = getters['channels/getChannelById'](selectedChannelId);
+
+    if (selectedChannel) {
+      const usersWhoSharesScreen = selectedChannel.users.filter(user => user.screen);
+
+      if (usersWhoSharesScreen.length > 0) {
+        const sortedSharings = usersWhoSharesScreen.sort((a, b) => Date.parse(b.startScreenTs || 0) - Date.parse(a.startScreenTs || 0));
+
+        lastUserWhoSharesMedia = sortedSharings[0].userId;
+
+        return sortedSharings[0].userId;
+      }
+    }
+  },
+
+  /**
    * Get user who share's screen or camera
    *
    * @param {object} state – global state
