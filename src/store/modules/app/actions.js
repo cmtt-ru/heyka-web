4fb60771f49db16d3d380128137d37f65a71342e
Michael Nalbandyan <xinger@ya.ru>
2021-03-15 19:36:31 +0300

    sync store

--- src/store/modules/app/actions.js
+++ src/store/modules/app/actions.js
@@ -156,4 +156,10 @@ export default {
       timestamp: Date.now(),
     });
   },
+
+  sendMiniChatMessage({ commit, rootGetters }, message) {
+    conversationBroadcast('mini-chat', rootGetters['me/getMyId'], {
+      message,
+    });
+  },
 };
